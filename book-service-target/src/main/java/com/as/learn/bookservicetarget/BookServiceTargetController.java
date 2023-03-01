99f022be551533535019ea82a58939ee8e345237
jobas2007 <jobas2007@gmail.com>
2023-03-01 03:42:18 -0500

    init commit

--- book-service-target/src/main/java/com/as/learn/bookservicetarget/BookServiceTargetController.java
+++ book-service-target/src/main/java/com/as/learn/bookservicetarget/BookServiceTargetController.java
@@ -1,16 +1,17 @@
 package com.as.learn.bookservicetarget;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/target/books")
+@RequestMapping("/target")
 @Slf4j
 public class BookServiceTargetController {
 
-    @PostMapping("/add")
+    @PostMapping
     public Book addNewBook(@RequestBody Book inBook){
         log.info("Incoming - Target Book:{}", inBook);
         Book book = new Book();
@@ -20,4 +21,21 @@ public class BookServiceTargetController {
         return book;
     }
 
+    @GetMapping
+    public List<Book> getBooks() {
+        log.info("Getting - Target Book");
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = new Book();
+        book1.setTitle("Target Title - JavaScript One");
+        book1.setAuthor("Target Author - Fredrick S");
+        books.add(book1);
+        //
+        Book book2 = new Book();
+        book2.setTitle("Target Title - Python One");
+        book2.setAuthor("Target Author - Micheal B");
+
+        books.add(book2);
+        return books;
+    }
 }
--- book-service-walmart/src/main/java/com/as/learn/bookservicewalmart/BookServiceWalmartController.java
+++ book-service-walmart/src/main/java/com/as/learn/bookservicewalmart/BookServiceWalmartController.java
@@ -1,17 +1,18 @@
 package com.as.learn.bookservicewalmart;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/walmart/books")
+@RequestMapping("/walmart")
 @Slf4j
 public class BookServiceWalmartController {
 
-    @PostMapping("/add")
-    public Book addNewBook(@RequestBody Book inBook){
+    @PostMapping
+    public Book addNewBook(@RequestBody Book inBook) {
         log.info("Incoming - Walmart Book:{}", inBook);
         Book book = new Book();
         book.setTitle(inBook.getTitle());
@@ -20,4 +21,21 @@ public class BookServiceWalmartController {
         return book;
     }
 
+    @GetMapping
+    public List<Book> getBooks() {
+        log.info("Getting - Walmart Book");
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = new Book();
+        book1.setTitle("Walmart Title - Spring One");
+        book1.setAuthor("Walmart Author - Ron J");
+        books.add(book1);
+        //
+        Book book2 = new Book();
+        book2.setTitle("Walmart Title - Java One");
+        book2.setAuthor("Walmart Author - James G");
+
+        books.add(book2);
+        return books;
+    }
 }
